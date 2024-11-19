7d1c9b897182abf700ba93149b9100cd9575a2d0
João Pedro Soares <joaopedrosoaresdebrito@gmail.com>
2024-11-19 10:20:35 -0300

    push: coleta de screenshots para evidencia

--- src/test/java/com/projetoiLAB/PageObject.java
+++ src/test/java/com/projetoiLAB/PageObject.java
@@ -2,6 +2,16 @@ package com.projetoiLAB;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class PageObject {
 
@@ -14,6 +24,26 @@ public class PageObject {
         } else {
             this.webDriver = webDriver;
         }
+    }
+
+    public void takeScreenShot(String test){
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm"));
+        String screenshotName = "evidences/screenshots/" + test + "_" + timestamp + ".png";
+
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        File screenshot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+
+        try {
+            Files.copy(screenshot.toPath(), Paths.get(screenshotName), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.err.println("Erro ao salvar o screenshot: " + e.getMessage());
+        }
+
     }
 
     public void close() {
--- src/test/java/com/projetoiLAB/tests/LoginTest.java
+++ src/test/java/com/projetoiLAB/tests/LoginTest.java
@@ -34,43 +34,52 @@ public class LoginTest {
     @Test
     public void CT01(){
         loginPage.fillLoginInputs("Admin", "admin123");
+        loginPage.takeScreenShot("CT01_CredenciaisPreenchidas");
         loginPage.submitLogin();
 
         Assertions.assertFalse(loginPage.isSelectedPage(URL_LOGIN_PAGE));
+        loginPage.takeScreenShot("CT01_DashboardPage");
         Assertions.assertTrue(loginPage.isSelectedPage(URL_DASHBOARD_PAGE));
     }
 
     @Test
     public void CT02(){
         loginPage.fillLoginInputs("Admin", "adm123");
+        loginPage.takeScreenShot("CT02_CredenciaisPreenchidas");
         loginPage.submitLogin();
 
         Assertions.assertTrue(loginPage.isSelectedPage(URL_LOGIN_PAGE));
         Assertions.assertTrue(loginPage.verifyMessage("Invalid credentials"));
+        loginPage.takeScreenShot("CT02_MensagemDeErro");
     }
 
     @Test
     public void CT03(){
         loginPage.fillLoginInputs("Administrador", "adm123");
+        loginPage.takeScreenShot("CT03_CredenciaisPreenchidas");
         loginPage.submitLogin();
 
         Assertions.assertTrue(loginPage.isSelectedPage(URL_LOGIN_PAGE));
         Assertions.assertTrue(loginPage.verifyMessage("Invalid credentials"));
+        loginPage.takeScreenShot("CT03_MensagemDeErro");
     }
 
     @Test
     public void CT04(){
         loginPage.fillLoginInputs("", "");
+        loginPage.takeScreenShot("CT04_CredenciaisEmBranco");
         loginPage.submitLogin();
 
         Assertions.assertTrue(loginPage.isSelectedPage(URL_LOGIN_PAGE));
         Assertions.assertTrue(loginPage.verifyMessage("Required"));
+        loginPage.takeScreenShot("CT04_MensagemDeErro");
         Assertions.assertTrue(loginPage.errorInput().isDisplayed());
     }
 
     @Test
     public void CT05(){
         loginPage.clickOnLinkForgot();
+        loginPage.takeScreenShot("CT05_ForgotMyPassword");
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
@@ -79,6 +88,7 @@ public class LoginTest {
 
         Assertions.assertFalse(loginPage.isSelectedPage(URL_LOGIN_PAGE));
         Assertions.assertTrue(loginPage.isSelectedPage(URL_RESETPASSWORD_PAGE));
+        loginPage.takeScreenShot("CT05_ResetPasswordPage");
         Assertions.assertTrue(loginPage.verifyMessage("Reset Password"));
         Assertions.assertTrue(loginPage.verifyMessage("Please enter your username to identify your account to reset your password"));
         Assertions.assertTrue(loginPage.verifyMessage("Reset Password"));
@@ -87,49 +97,59 @@ public class LoginTest {
     @Test
     public void CT06(){
         loginPage.clickOnLink(URL_LINKEDIN);
+        loginPage.takeScreenShot("CT06_ClickLinkedin");
 
         Set<String> abasAbertas = loginPage.getWindowHandles();
         Assertions.assertEquals(3, abasAbertas.size());
         Assertions.assertTrue(loginPage.switchTab(URL_LOGIN_PAGE));
         Assertions.assertTrue(loginPage.switchTab("https://br.linkedin.com/company/orangehrm"));
+        loginPage.takeScreenShot("CT06_LinkedinPage");
     }
     @Test
     public void CT07(){
         loginPage.clickOnLink(URL_YOUTUBE);
+        loginPage.takeScreenShot("CT07_ClickYoutube");
 
         Set<String> abasAbertas = loginPage.getWindowHandles();
         Assertions.assertEquals(3, abasAbertas.size());
         Assertions.assertTrue(loginPage.switchTab(URL_LOGIN_PAGE));
         Assertions.assertTrue(loginPage.switchTab("https://www.youtube.com/c/OrangeHRMInc"));
+        loginPage.takeScreenShot("CT07_YoutubePage");
     }
 
     @Test
     public void CT08(){
         loginPage.clickOnLink(URL_FACEBOOK);
+        loginPage.takeScreenShot("CT08_ClickFacebook");
 
         Set<String> abasAbertas = loginPage.getWindowHandles();
         Assertions.assertEquals(3, abasAbertas.size());
         Assertions.assertTrue(loginPage.switchTab(URL_LOGIN_PAGE));
         Assertions.assertTrue(loginPage.switchTab("https://www.facebook.com/OrangeHRM/"));
+        loginPage.takeScreenShot("CT08_FacebookPage");
     }
 
     @Test
     public void CT09(){
         loginPage.clickOnLink(URL_TWITTER);
+        loginPage.takeScreenShot("CT09_ClickTwitter");
 
         Set<String> abasAbertas = loginPage.getWindowHandles();
         Assertions.assertEquals(3, abasAbertas.size());
         Assertions.assertTrue(loginPage.switchTab(URL_LOGIN_PAGE));
         Assertions.assertTrue(loginPage.switchTab("https://x.com/orangehrm?lang=en"));
+        loginPage.takeScreenShot("CT09_TwitterPage");
     }
 
     @Test
     public void CT10(){
         loginPage.clickOnLink(URL_ORANGEHRM);
+        loginPage.takeScreenShot("CT10_ClickOrangeHRM");
 
         Set<String> abasAbertas = loginPage.getWindowHandles();
         Assertions.assertEquals(3, abasAbertas.size());
         Assertions.assertTrue(loginPage.switchTab(URL_LOGIN_PAGE));
         Assertions.assertTrue(loginPage.switchTab("https://www.orangehrm.com/"));
+        loginPage.takeScreenShot("CT10_OrangeHRMPage");
     }
 }
